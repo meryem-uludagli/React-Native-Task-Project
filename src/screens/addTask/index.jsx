90ab441b07b86e91466b3358641f15b88b1c9868
meryem-uludagli <meryemuslu2035@gmail.com>
2024-10-22 20:08:15 +0300

    first commit

---
+++ src/screens/addTask/index.jsx
@@ -0,0 +1,91 @@
+import {Input, Button, RadioGroup, Radio} from '@ui-kitten/components';
+import {Formik} from 'formik';
+import {Alert, StyleSheet, Text, View} from 'react-native';
+import CustomDatePicker from '../../components/ui/customDatePicker';
+import taskSchema from '../../utils/validation';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const AddTask = () => {
+  return (
+    <View style={styles.container}>
+      <Formik
+        initialValues={{
+          title: '',
+          description: '',
+          startDate: null,
+          endDate: null,
+          category: null,
+        }}
+        validationSchema={taskSchema}
+        onSubmit={values => Alert.alert(JSON.stringify(values, null, 2))}>
+        {({handleChange, handleSubmit, values, setFieldValue, errors}) => (
+          <View>
+            <Input
+              size="large"
+              style={{marginVertical: 10}}
+              value={values.title}
+              label={'Title'}
+              placeholder=""
+              onChangeText={handleChange('title')}
+              status={errors.title ? 'danger' : 'basic'}
+              caption={errors.title}
+            />
+            <Input
+              multiline
+              size="large"
+              style={{marginVertical: 10}}
+              value={values.description}
+              label={'Description'}
+              placeholder=""
+              onChangeText={handleChange('description')}
+              status={errors.description ? 'danger' : 'basic'}
+              caption={errors.description}
+            />
+
+            <CustomDatePicker
+              size="large"
+              style={{marginVertical: 10}}
+              date={values.startDate}
+              label={'Start Date'}
+              onSelectDate={date => setFieldValue('startDate', date)}
+              status={errors.startDate ? 'danger' : 'basic'}
+              caption={errors.startDate}
+            />
+            <CustomDatePicker
+              size="large"
+              style={{marginVertical: 10}}
+              date={values.endDate}
+              label={'End Date'}
+              onSelectDate={date => setFieldValue('endDate', date)}
+              status={errors.endDate ? 'danger' : 'basic'}
+              caption={errors.endDate}
+            />
+
+            <RadioGroup
+              selectedIndex={values.category}
+              onChange={index => setFieldValue('category', index)}>
+              <Radio status="success">Software</Radio>
+              <Radio status="success">Design</Radio>
+              <Radio status="success">Operation</Radio>
+            </RadioGroup>
+            <Button
+              status="success"
+              style={{marginTop: 30}}
+              onPress={handleSubmit}>
+              CREATE
+            </Button>
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
+};
+
+export default AddTask;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 15,
+  },
+});
---
+++ src/screens/home/index.jsx
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import FloatActionButton from '../../components/ui/floatActionButton';
+import {ADDTASKS} from '../../utils/routes';
+
+const Home = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+      <FloatActionButton onPress={() => navigation.navigate(ADDTASKS)} />
+    </View>
+  );
+};
+
+export default Home;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
